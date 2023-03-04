d304c9df48a09d2373082b85e4f80f579dab20a0
Justin <justinporter651@gmail.com>
2023-03-03 18:14:04 -0600

    Fixed all the web dependencies and updated all vulnerabilities

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  i18n: {
+    locales: ["en-US", "fr", "nl-NL"],
+    defaultLocale: "en-US"
+  },
+  experimental: {
+    urlImports: [
+      "https://framer.com/m/",
+      "https://framerusercontent.com/",
+      "https://ga.jspm.io/",
+      "https://jspm.dev/"
+    ]
+  }
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import CUButton from "../components/contactUsButton";
+import Footer from "../components/footer";
+import { useRouter } from "next/router";
+import { analytics, auth } from "../lib/firebase";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const routers = useRouter();
+
+  useEffect(() => {
+    const logEvent = url => {
+      analytics().setCurrentScreen(url);
+      analytics().logEvent("screen_view");
+    };
+
+    routers.events.on("routeChangeComplete", logEvent);
+    //For First Page
+    logEvent(window.location.pathname);
+
+    //Remvove Event Listener after un-mount
+    return () => {
+      routers.events.off("routeChangeComplete", logEvent);
+    };
+  }, []);
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <CUButton />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
